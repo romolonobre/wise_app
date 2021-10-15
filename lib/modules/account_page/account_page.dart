e9a5bde27e128ad630d841813356592519486e22
Romolo Nobre <romoloferreira@icloud.com>
2021-10-15 10:01:10 +0100

    Added  Scroll

--- lib/modules/account_page/account_page.dart
+++ lib/modules/account_page/account_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:wise/modules/account_page/widgets/account_app_bar.dart';
 import 'package:wise/modules/account_page/widgets/profile_avatar.dart';
+import 'package:wise/modules/account_page/widgets/profile_name_header.dart';
 import 'package:wise/modules/home_bottom_bar_page.dart/widgets/custom_app_bar.dart';
 
 class AccountPage extends StatelessWidget {
@@ -12,13 +13,17 @@ class AccountPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: const AccountAppBar(),
-      body: Container(
-        height: Get.height,
-        width: Get.width,
-        child: Column(
-          children: const [
-            ProfileAvatar(),
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          height: Get.height,
+          width: Get.width,
+          child: Column(
+            children: const [
+              ProfileAvatar(),
+              SizedBox(height: 10),
+              ProfileNameHeader(),
+            ],
+          ),
         ),
       ),
     );
