dd5d38c1f165df7bf2e2a8c44947193baf74fd50
Romolo Nobre <romoloferreira@icloud.com>
2021-10-17 16:03:57 +0100

    modified AccountPage

--- lib/modules/account_page/account_page.dart
+++ lib/modules/account_page/account_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:wise/modules/account_page/widgets/account_app_bar.dart';
+import 'package:wise/modules/account_page/widgets/icon_card_functionality.dart';
 import 'package:wise/modules/account_page/widgets/profile_avatar.dart';
 import 'package:wise/modules/account_page/widgets/profile_name_header.dart';
 import 'package:wise/modules/account_page/widgets/wise_bank_card_list_view.dart';
@@ -19,12 +20,24 @@ class AccountPage extends StatelessWidget {
           height: Get.height,
           width: Get.width,
           child: Column(
-            children: const [
-              ProfileAvatar(),
-              SizedBox(height: 10),
-              ProfileNameHeader(),
-              SizedBox(height: 10),
-              WiseBankCardListView()
+            children: [
+              const ProfileAvatar(),
+              const SizedBox(height: 10),
+              const ProfileNameHeader(),
+              const SizedBox(height: 10),
+              const WiseBankCardListView(),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  IconCardFunctionality(
+                      icon: Icon(Icons.ac_unit), label: 'Freeze card'),
+                  IconCardFunctionality(
+                      icon: Icon(Icons.credit_card), label: 'Card details'),
+                  IconCardFunctionality(
+                      icon: Icon(Icons.settings), label: 'Manage card'),
+                ],
+              )
             ],
           ),
         ),
