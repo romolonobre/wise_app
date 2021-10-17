3c8597a175d924332bc8d23748ab4ecb46d37d19
Romolo Nobre <romoloferreira@icloud.com>
2021-10-17 16:02:45 +0100

    Modified widget

--- lib/modules/account_page/widgets/wise_bank_card_list_view.dart
+++ lib/modules/account_page/widgets/wise_bank_card_list_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'wise_card_caontent.dart';
+
 class WiseBankCardListView extends StatelessWidget {
   const WiseBankCardListView({Key? key}) : super(key: key);
 
@@ -44,94 +46,6 @@ class WiseBankCardListView extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class Wise_Card_Content extends StatelessWidget {
-  final String imagePath;
-  final Color cardColor;
-  final String cardNumber;
-  final String expiryDate;
-  final String cardType;
-  final double topBottom;
-  final double leftRigh;
-
-  const Wise_Card_Content({
-    Key? key,
-    required this.imagePath,
-    required this.cardColor,
-    required this.cardNumber,
-    required this.expiryDate,
-    required this.cardType,
-    required this.topBottom,
-    required this.leftRigh,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 20),
-          width: Get.width * 0.6,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10), color: cardColor),
-        ),
-        Positioned(
-          left: leftRigh,
-          top: topBottom,
-          child: CircleAvatar(
-            backgroundColor: Colors.white,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Image.asset(
-                imagePath,
-                height: 70,
-                width: 200,
-                fit: BoxFit.contain,
-              ),
-              const SizedBox(height: 4),
-              Text(
-                cardNumber,
-                style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600),
-              ),
-            ],
-          ),
-        ),
-        Positioned(
-          bottom: 10,
-          right: 40,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                expiryDate,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              Text(cardType,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w800,
-                      fontSize: 19))
-            ],
-          ),
-        )
-      ],
     );
   }
 }
